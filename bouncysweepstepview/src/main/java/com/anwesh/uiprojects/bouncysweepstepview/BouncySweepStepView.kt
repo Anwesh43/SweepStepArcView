62b663f3aeadc3033db0f5ccf9c38487eb253626
Anwesh43 <anweshthecool0@gmail.com>
2020-01-13 03:31:34 +0530

    adding BouncySweepStepArc class

--- bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
+++ bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
@@ -156,4 +156,27 @@ class BouncySweepStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BouncySweepStepArc(var i : Int) {
+
+        private var curr : SSANode = SSANode(i + 1)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
