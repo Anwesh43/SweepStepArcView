10f27daf3086d7c7692b0a71e761e357da687169
Anwesh43 <anweshthecool0@gmail.com>
2020-01-13 03:38:11 +0530

    adding Renderer class

--- bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
+++ bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
@@ -54,15 +54,16 @@ fun Canvas.drawSSANode(i : Int, scale : Float, paint : Paint) {
 class BouncySweepStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
