55a4f88648e448eac1a0b60e51f2e428c8c46300
Anwesh43 <anweshthecool0@gmail.com>
2020-01-13 03:29:54 +0530

    adding SSANode class

--- bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
+++ bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
@@ -115,4 +115,45 @@ class BouncySweepStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSANode(var i : Int, val state : State = State()) {
+
+        private var next : SSANode? = null
+        private var prev : SSANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SSANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSANode {
+            var curr : SSANode? = prev
+            if(dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
