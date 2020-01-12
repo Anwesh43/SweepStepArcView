dc4f0801cbacab0ee4cba6dbe920ccb91eecdbfb
Anwesh43 <anweshthecool0@gmail.com>
2020-01-13 03:04:45 +0530

    adding constants and libraries

--- bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
+++ bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.bouncysweepstepview
 /**
  * Created by anweshmishra on 13/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val steps : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
