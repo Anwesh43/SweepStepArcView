055fc62e84c1cee5d0fb2d568f5b3efce5103712
Anwesh43 <anweshthecool0@gmail.com>
2020-01-13 03:22:25 +0530

    adding BouncySweepStepView class

--- bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
+++ bouncysweepstepview/src/main/java/com/anwesh/uiprojects/bouncysweepstepview/BouncySweepStepView.kt
@@ -50,3 +50,21 @@ fun Canvas.drawSSANode(i : Int, scale : Float, paint : Paint) {
     drawSweepStepArc(w, scale, paint)
     restore()
 }
+
+class BouncySweepStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
